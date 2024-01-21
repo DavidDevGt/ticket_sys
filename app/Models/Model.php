42f9856933c9584435c0b930b1bbd5125170efb1
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-21 02:18:39 -0600

    Refactor Model class to include save, update, and delete methods

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -1,17 +1,45 @@
 <?php
-abstract class Model {
+abstract class Model
+{
     protected $conn;
     protected $table;
 
-    public function __construct($conn) {
+    public function __construct($conn)
+    {
         $this->conn = $conn;
     }
 
-    public function getAll() {
+    public function getAll()
+    {
         $query = "SELECT * FROM " . $this->table;
         $result = $this->conn->query($query);
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
     // Otros métodos podrían ser: (save, update, delete)
+
+    public function save($data)
+    {
+        $fields = implode(', ', array_keys($data));
+        $values = implode(", ", array_map(function ($value) {
+            return "'" . $this->conn->real_escape_string($value) . "'";
+        }, array_values($data)));
+        $query = "INSERT INTO {$this->table} ($fields) VALUES ($values)";
+        return $this->conn->query($query);
+    }
+
+    public function update($id, $data)
+    {
+        $fields = implode(", ", array_map(function ($key, $value) {
+            return "$key = '" . $this->conn->real_escape_string($value) . "'";
+        }, array_keys($data), array_values($data)));
+        $query = "UPDATE {$this->table} SET $fields WHERE id = $id";
+        return $this->conn->query($query);
+    }
+
+    public function delete($id)
+    {
+        $query = "UPDATE {$this->table} SET active = false WHERE id = $id";
+        return $this->conn->query($query);
+    }
 }
