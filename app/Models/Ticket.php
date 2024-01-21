93d60eb1106c8d4ef01ca35b092baf08f1377bad
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-21 09:10:08 -0600

    Add getters and setters for Ticket model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -25,4 +25,81 @@ class Ticket extends Model
         $this->active = $active;
     }
 
+    // Getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTitulo() {
+        return $this->titulo;
+    }
+
+    public function getDescripcion() {
+        return $this->descripcion;
+    }
+
+    public function getClienteId() {
+        return $this->cliente_id;
+    }
+
+    public function getEstado() {
+        return $this->estado;
+    }
+
+    public function getFechaCreacion() {
+        return $this->fecha_creacion;
+    }
+
+    public function getFechaEdicion() {
+        return $this->fecha_edicion;
+    }
+
+    public function getUsuarioCreacionId() {
+        return $this->usuario_creacion_id;
+    }
+
+    public function getUsuarioEdicionId() {
+        return $this->usuario_edicion_id;
+    }
+
+    public function isActive() {
+        return $this->active;
+    }
+
+    // Setters
+    public function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+
+    public function setDescripcion($descripcion) {
+        $this->descripcion = $descripcion;
+    }
+
+    public function setClienteId($cliente_id) {
+        $this->cliente_id = $cliente_id;
+    }
+
+    public function setEstado($estado) {
+        $this->estado = $estado;
+    }
+
+    public function setFechaCreacion($fecha_creacion) {
+        $this->fecha_creacion = $fecha_creacion;
+    }
+
+    public function setFechaEdicion($fecha_edicion) {
+        $this->fecha_edicion = $fecha_edicion;
+    }
+
+    public function setUsuarioCreacionId($usuario_creacion_id) {
+        $this->usuario_creacion_id = $usuario_creacion_id;
+    }
+
+    public function setUsuarioEdicionId($usuario_edicion_id) {
+        $this->usuario_edicion_id = $usuario_edicion_id;
+    }
+
+    public function setActive($active) {
+        $this->active = $active;
+    }
 }
