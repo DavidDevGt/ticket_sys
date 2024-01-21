af60eef35ce4723fcdcda5f15f2fc18f01a5ec02
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-21 13:52:01 -0600

    Add session configuration to index.php8

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/Session.php';
+
+// Session config
+use Config\Session;
+Session::start();
+
+// ENV
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
