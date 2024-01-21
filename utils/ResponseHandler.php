4469b1d2af87a025c600087b592043d4e40098e6
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-21 11:46:52 -0600

    Add Utils namespace and ResponseHandler class

---
+++ utils/ResponseHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Utils;
+
+class ResponseHandler
+{
+    public static function sendResponse($data, $statusCode = 200)
+    {
+        header('Content-Type: application/json');
+        http_response_code($statusCode);
+        echo json_encode($data);
+        exit;
+    }
+}
