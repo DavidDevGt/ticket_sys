3705e8067365ad9696d902cac32fd630d8060a81
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-21 01:42:54 -0600

    Add RolController class

---
+++ app/Controllers/RolController.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'Controller.php';
+require_once '../Models/Rol.php';
+
+// Controlador específico para 'Rol'
+class RolController extends Controller {
+    public function __construct() {
+        // $this->model = new Rol();
+    }
+
+    // Métodos específicos para el controlador de Rol
+    // Ejemplo: public function createRol() { ... }
+}
