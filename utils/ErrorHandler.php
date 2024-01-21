55e4e7433f2a0184b59ee429f909d7ae8af1d926
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-21 12:28:30 -0600

    Add error handling methods to ErrorHandler class

--- utils/ErrorHandler.php
+++ utils/ErrorHandler.php
@@ -12,4 +12,39 @@ class ErrorHandler
     {
         self::handleError('Not Found', 404);
     }
+
+    public static function badRequest()
+    {
+        self::handleError('Bad Request', 400);
+    }
+
+    public static function unauthorized()
+    {
+        self::handleError('Unauthorized', 401);
+    }
+
+    public static function forbidden()
+    {
+        self::handleError('Forbidden', 403);
+    }
+
+    public static function methodNotAllowed()
+    {
+        self::handleError('Method Not Allowed', 405);
+    }
+
+    public static function conflict()
+    {
+        self::handleError('Conflict', 409);
+    }
+
+    public static function internalServerError()
+    {
+        self::handleError('Internal Server Error', 500);
+    }
+
+    public static function serviceUnavailable()
+    {
+        self::handleError('Service Unavailable', 503);
+    }
 }
